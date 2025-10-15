fae7002d31658cbf4f62f40dc8ff3961e0842459
Deemazmm <s202261140@kfupm.edu.sa>
2025-10-15 12:00:39 +0300

    1.5

--- 5-6-react-styling-fetch-data/src/components/UserCard.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserCard.jsx
@@ -18,7 +18,12 @@ function UserCard({ user, onUserClick }) {
           <strong>Phone:</strong> {user.phone}
         </Card.Text>
       {/* Buttotn*/}
-      
+      <Button 
+          variant="primary" 
+          onClick={() => onUserClick(user)}
+        >
+          View Details
+        </Button>
       </Card.Body>
     </Card>
   )
