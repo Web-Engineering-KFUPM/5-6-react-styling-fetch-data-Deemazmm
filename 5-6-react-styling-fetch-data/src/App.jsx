156609672076054a05a32c1c5b0ee28834778482
Deemazmm <s202261140@kfupm.edu.sa>
2025-10-15 11:57:22 +0300

    todo1.2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -338,10 +338,10 @@ function App() {
 
   return (
     <div className="app">
-      <header className="py-3 mb-4">
+      <header className="bg-primary text-white py-3 mb-4 shadow">
         <Container>
-          <h1 className="">User Management Dashboard</h1>
-          <p className="">Manage and view user information</p>
+          <h1 className="h2 mb-0">User Management Dashboard</h1>
+          <p className="mb-0 opacity-75">Manage and view user information</p>
         </Container>
       </header>
 
